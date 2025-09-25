730ba7725d87b2b85eb9a6e3a6dba292f147404f
Tokomichel <tokowouendeu@gmail.com>
2025-09-25 23:12:47 +0200

    fix: creation de la partie admin de place

--- klaces_backend/manage_places/admin.py
+++ klaces_backend/manage_places/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Place
 
+
+class Place_register(admin.ModelAdmin):
+    list_display = ["id", "location", "place_type", "etoiles"]
+
 # Register your models here.
 
-admin.site.register(Place)
+admin.site.register(Place, Place_register)
