26f0477cb273d984d6d7dcf223df7949a137352b
Tokomichel <tokowouendeu@gmail.com>
2025-09-27 14:55:03 +0200

    feat: ajout du get avec ID

--- klaces_backend/manage_places/urls.py
+++ klaces_backend/manage_places/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import Api_place
-
+from .views import Api_place, get_one_place
 
 urlpatterns = [
- path("", Api_place.as_view())
+ path("", Api_place.as_view()),
+ path("<int:id_place>", get_one_place)
 ]
--- klaces_backend/manage_places/views.py
+++ klaces_backend/manage_places/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponse
 from rest_framework.response import Response
 from rest_framework.request import Request
+from rest_framework.decorators import api_view
 from rest_framework import status
 from .models import *
 from rest_framework.views import APIView
@@ -10,7 +11,18 @@ from rest_framework.views import APIView
 
 
 # Create your views here.
+@api_view(['GET'])
+def get_one_place(req: Request, id_place: int):
+    place = Place.objects.get(id=id_place)
 
+    json_place = {
+        "name": place.name,
+        "location": place.location,
+        "place type": place.place_type,
+        "etoiles": place.etoiles
+    }
+
+    return Response(data=json_place, status=200)
 
 class Api_place(APIView):
 
@@ -33,3 +45,13 @@ class Api_place(APIView):
                data.append(meta)
 
            return Response(data=data, status=status.HTTP_200_OK)
+
+    def post(self, req: Request):
+
+        place = Place()
+        place.name = req.data["name"]
+        place.location = req.data["location"]
+        place.etoiles = req.data["etoiles"]
+        place.place_type = req.data["place_type"]
+
+        place.save()
